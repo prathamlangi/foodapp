18af785ee7e854226e1b8f616bf98de5c1e4d228
pratham langi <prathamlangi00@gmail.com>
2023-08-25 15:09:41 +0530

    navigation view not compeleted

--- app/src/main/java/com/example/foodapp/LoginActivity.kt
+++ app/src/main/java/com/example/foodapp/LoginActivity.kt
@@ -34,16 +34,16 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         //creating shared preference object
-        sharedPreferences =
-            getSharedPreferences(getString(R.string.preference_file_name), Context.MODE_PRIVATE)
-
-        //checking by SharedPreferences is loggedIn or not
-        val isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false)
-        if (isLoggedIn) {
-            val i = Intent(this@LoginActivity, MainActivity::class.java)
-            startActivity(i)
-            finish()
-        }
+//        sharedPreferences =
+//            getSharedPreferences(getString(R.string.preference_file_name), Context.MODE_PRIVATE)
+//
+//        //checking by SharedPreferences is loggedIn or not
+//        val isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false)
+//        if (isLoggedIn) {
+//            val i = Intent(this@LoginActivity, MainActivity::class.java)
+//            startActivity(i)
+//            finish()
+//        }
 
         enterPhone = findViewById(R.id.et_phone)
         enterPass = findViewById(R.id.et_password)
@@ -53,11 +53,13 @@ class LoginActivity : AppCompatActivity() {
 
 
         btnLogin.setOnClickListener {
-            val menterPhone = enterPhone.text.toString()
-            val menterPass = enterPass.text.toString()
-            if (validateInput(menterPhone, menterPass)) {
-                authenticateWithAPI(menterPhone, menterPass)
-            }
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+//            val menterPhone = enterPhone.text.toString()
+//            val menterPass = enterPass.text.toString()
+//            if (validateInput(menterPhone, menterPass)) {
+//                authenticateWithAPI(menterPhone, menterPass)
+//            }
         }
 
 //        Go to Forget Activity
@@ -74,94 +76,94 @@ class LoginActivity : AppCompatActivity() {
     }
 
     //Check length of input mobile number and password
-    private fun validateInput(menterPhone: String, menterPass: String): Boolean {
-        if (menterPhone.length != 10) {
-            Toast.makeText(this@LoginActivity, "Enter Valid Number", Toast.LENGTH_SHORT).show()
-            return false
-        }
-        if (menterPass.length <= 4) {
-            Toast.makeText(this@LoginActivity, "Enter Valid Password", Toast.LENGTH_SHORT).show()
-            return false
-        }
-        return true
-    }
-
-    //authenticateWithAPI
-    private fun authenticateWithAPI(menterPhone: String, menterPass: String) {
-        val queue = Volley.newRequestQueue(this@LoginActivity)
-        val url = "http://13.235.250.119/v2/login/fetch_result/"
-
-        val jsonParams = JSONObject()
-        jsonParams.put("mobile_number", menterPhone)
-        jsonParams.put("password", menterPass)
-
-        if (ConnectionManager().checkConnectivity(this@LoginActivity)) {
-
-            val request =
-                object : JsonObjectRequest(Request.Method.POST, url, jsonParams, Response.Listener {
-                    println("response $it")
-                    try {
-                        val dataRequest = it.getJSONObject("data")
-                        val success = it.getBoolean("success")
-                        if (success) {
-                            val dataResponse = dataRequest.getJSONObject("data")
-                            savePreference(dataResponse)
-                            val i = Intent(this@LoginActivity, MainActivity::class.java)
-                            startActivity(i)
-                        } else {
-                            Toast.makeText(
-                                this@LoginActivity,
-                                "Not getting true  Error Occurred",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                    } catch (e: Exception) {
-                        Toast.makeText(
-                            this@LoginActivity, "Some Error Occurred!!!!", Toast.LENGTH_SHORT
-                        ).show()
-                    }
-
-
-                }, Response.ErrorListener {
-                    Toast.makeText(
-                        this@LoginActivity, "Getting Volley Error ", Toast.LENGTH_SHORT
-                    ).show()
-
-                }) {
-                    override fun getHeaders(): MutableMap<String, String> {
-                        val headers = HashMap<String, String>()
-                        headers["Content-type"] = "application/json"
-                        headers["token"] = "637e8feaae4684"
-                        return headers
-                    }
-                }
-            queue.add(request)
-        } else {
-            val dialog = AlertDialog.Builder(this@LoginActivity)
-            dialog.setTitle("Error")
-            dialog.setMessage("Internet Connection is not Found")
-            dialog.setPositiveButton("Open Setting") { _, _ ->
-                val settingIntent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
-                startActivity(settingIntent)
-                finish()
-            }
-            dialog.setNegativeButton("Exit") { _, _ ->
-                ActivityCompat.finishAffinity(this@LoginActivity)
-            }
-            dialog.create()
-            dialog.show()
-
-        }
-    }
-
-    fun savePreference(userData: JSONObject) {
-        sharedPreferences.edit().putBoolean("isLoggedIn", true).apply()
-        sharedPreferences.edit().putString("user_id", userData.getString("user_id")).apply()
-        sharedPreferences.edit().putString("name", userData.getString("name")).apply()
-        sharedPreferences.edit().putString("email", userData.getString("email")).apply()
-        sharedPreferences.edit().putString("mobile_number", userData.getString("mobile_number"))
-            .apply()
-        sharedPreferences.edit().putString("address", userData.getString("address")).apply()
-    }
+//    private fun validateInput(menterPhone: String, menterPass: String): Boolean {
+//        if (menterPhone.length != 10) {
+//            Toast.makeText(this@LoginActivity, "Enter Valid Number", Toast.LENGTH_SHORT).show()
+//            return false
+//        }
+//        if (menterPass.length <= 4) {
+//            Toast.makeText(this@LoginActivity, "Enter Valid Password", Toast.LENGTH_SHORT).show()
+//            return false
+//        }
+//        return true
+//    }
+//
+//    //authenticateWithAPI
+//    private fun authenticateWithAPI(menterPhone: String, menterPass: String) {
+//        val queue = Volley.newRequestQueue(this@LoginActivity)
+//        val url = "http://13.235.250.119/v2/login/fetch_result/"
+//
+//        val jsonParams = JSONObject()
+//        jsonParams.put("mobile_number", menterPhone)
+//        jsonParams.put("password", menterPass)
+//
+//        if (ConnectionManager().checkConnectivity(this@LoginActivity)) {
+//
+//            val request =
+//                object : JsonObjectRequest(Request.Method.POST, url, jsonParams, Response.Listener {
+//                    println("response $it")
+//                    try {
+//                        val dataRequest = it.getJSONObject("data")
+//                        val success = it.getBoolean("success")
+//                        if (success) {
+//                            val dataResponse = dataRequest.getJSONObject("data")
+//                            savePreference(dataResponse)
+//                            val i = Intent(this@LoginActivity, MainActivity::class.java)
+//                            startActivity(i)
+//                        } else {
+//                            Toast.makeText(
+//                                this@LoginActivity,
+//                                "Not getting true  Error Occurred",
+//                                Toast.LENGTH_SHORT
+//                            ).show()
+//                        }
+//                    } catch (e: Exception) {
+//                        Toast.makeText(
+//                            this@LoginActivity, "Some Error Occurred!!!!", Toast.LENGTH_SHORT
+//                        ).show()
+//                    }
+//
+//
+//                }, Response.ErrorListener {
+//                    Toast.makeText(
+//                        this@LoginActivity, "Getting Volley Error ", Toast.LENGTH_SHORT
+//                    ).show()
+//
+//                }) {
+//                    override fun getHeaders(): MutableMap<String, String> {
+//                        val headers = HashMap<String, String>()
+//                        headers["Content-type"] = "application/json"
+//                        headers["token"] = "637e8feaae4684"
+//                        return headers
+//                    }
+//                }
+//            queue.add(request)
+//        } else {
+//            val dialog = AlertDialog.Builder(this@LoginActivity)
+//            dialog.setTitle("Error")
+//            dialog.setMessage("Internet Connection is not Found")
+//            dialog.setPositiveButton("Open Setting") { _, _ ->
+//                val settingIntent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
+//                startActivity(settingIntent)
+//                finish()
+//            }
+//            dialog.setNegativeButton("Exit") { _, _ ->
+//                ActivityCompat.finishAffinity(this@LoginActivity)
+//            }
+//            dialog.create()
+//            dialog.show()
+//
+//        }
+//    }
+//
+//    fun savePreference(userData: JSONObject) {
+//        sharedPreferences.edit().putBoolean("isLoggedIn", true).apply()
+//        sharedPreferences.edit().putString("user_id", userData.getString("user_id")).apply()
+//        sharedPreferences.edit().putString("name", userData.getString("name")).apply()
+//        sharedPreferences.edit().putString("email", userData.getString("email")).apply()
+//        sharedPreferences.edit().putString("mobile_number", userData.getString("mobile_number"))
+//            .apply()
+//        sharedPreferences.edit().putString("address", userData.getString("address")).apply()
+//    }
 
 }
--- app/src/main/java/com/example/foodapp/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -2,12 +2,58 @@ package com.example.foodapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.FrameLayout
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var coordinatorlayout: CoordinatorLayout
+    private lateinit var toolbar: Toolbar
+    private lateinit var frameLayout: FrameLayout
+    private lateinit var navigationView: NavigationView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        drawerLayout = findViewById(R.id.drawerlayout)
+        coordinatorlayout = findViewById(R.id.coordinatorlayout)
+        toolbar = findViewById(R.id.my_toolbar)
+        frameLayout = findViewById(R.id.framelayout)
+        navigationView = findViewById(R.id.navigationlayout)
+        setToolBar()
+
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this@MainActivity,
+            drawerLayout,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
     }
+    private fun setToolBar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Toolbar Title"
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == android.R.id.home) {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
